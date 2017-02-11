b4949761c59fddd9e1074ad362c6b999ece6a93f
jonduan <jonduan@uvic.ca>
2017-02-10 23:19:55 -0800

    update changes for UVic ECON

---
+++ prepare.R
@@ -0,0 +1,10 @@
+install.packages("Zelig")
+install.packages("stargazer")
+install.packages("RefManageR")
+install.packages("knitcitations")
+install.packages("rcrossref")
+
+
+# miktex install abstract.sty, ulem.sty , lastpage.sty , fontawesomeetc.
+
+# change template path in yaml by using your working directory
